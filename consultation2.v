23e504a0c78d2284696b38b56b9cb773aa849b09
Bodor Andras <bodri@kvm-stretch>
2022-11-29 15:15:52 +0100

    c4

--- consultation2.v
+++ consultation2.v
@@ -6,6 +6,7 @@ Hypothesis p1: (M\/N)/\(O\/P).
 Hypothesis p2: N -> P.
 Hypothesis p3: ~P.
 Goal M/\O.
+Proof.
 unfold not in p3.
 pose proof (proj1 p1) as H1.
 pose proof (proj2 p1) as H2.
@@ -20,3 +21,53 @@ pose proof (p3 H3) as Hfalse.
 destruct Hfalse.
 Qed.
 End Exercise13.
+
+Section Exercise12.
+Variables S W: Prop.
+Hypothesis p1: S <-> W.
+Goal S <-> (W/\S).
+Proof.
+  assert (S -> W/\S) as H4. intros H1.
+  pose proof (H1) as H2.
+  rewrite p1 in H2.
+  pose proof (conj H2 H1) as H3.
+  exact H3.
+  assert (W/\S -> S) as H5. intros H1.
+  pose proof (proj2 H1) as H2.
+  exact H2.
+  pose proof (conj H4 H5: S <-> W /\ S) as H6.
+  exact H6.
+Qed.
+End Exercise12.
+
+
+Section Exercise12mostlybackwards.
+Variables S W: Prop.
+Hypothesis p1: S <-> W.
+Goal S <-> (W/\S).
+Proof.
+  split.
+  intros H1.
+  split.
+  rewrite <- p1.
+  exact H1.
+  exact H1.
+ intros H1.
+  pose proof (proj2 H1) as H2.
+(*this was a forward step*)
+  exact H2.
+Qed.
+End Exercise12mostlybackwards.
+
+
+
+
+
+
+
+
+
+
+
+
+
