2ca77fa93470808d90df83acbdf36e6f094ad58f
Bodor Andras <bodri@ribizli>
2022-12-06 17:19:26 +0100

    place for the exam questions

---
+++ coqideMidterm2.v
@@ -0,0 +1,19 @@
+(* Mathematical Logic second midterm
+   computer exercises
+
+Your Name:
+
+Your Neptun code:
+
+Send back the solutions to: bodri.mh3@gmail.com
+
+*)
+
+(* It is enough to solve any three of these
+exercises to get maximum points for the computer part
+of the exam. *)
+
+
+(* The exercises will be here when they are uploaded
+   at the beginning of the exam. *)
+
