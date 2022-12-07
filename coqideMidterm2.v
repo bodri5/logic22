b53b27807e0b24578a81c3a4254e6baaa529733b
Bodor Andras <bodri@kvm-stretch>
2022-12-07 10:08:45 +0100

    live exam

--- coqideMidterm2.v
+++ coqideMidterm2.v
@@ -5,7 +5,9 @@ Your Name:
 
 Your Neptun code:
 
-Send back the solutions to: bodri.mh3@gmail.com
+Send back the solutions to
+bodri.mh3@gmail.com
+
 
 *)
 
@@ -13,7 +15,64 @@ Send back the solutions to: bodri.mh3@gmail.com
 exercises to get maximum points for the computer part
 of the exam. *)
 
+Require Import Classical.
+Require Import Setoid.
 
-(* The exercises will be here when they are uploaded
-   at the beginning of the exam. *)
+Parameters P Q R S E: Prop.
+
+Section exercise1a.
+Hypothesis pr1: P -> Q.
+Hypothesis pr2: Q /\ (R \/ P).
+Hypothesis pr3: ~(Q \/ R).
+Goal False.
+
+(*Write your solution here.*)
+
+Abort. (*You can replace Abort with Qed
+         when finished with the proof.*)
+End exercise1a.
+
+Section exercise1b.
+Hypothesis pr1: ~(P<->Q).
+Goal ~(P/\Q).
+
+(*Write your solution here.*)
+
+Abort.
+End exercise1b.
+
+
+Section exercise1c.
+Hypothesis pr1: P /\ (Q \/ R).
+Hypothesis pr2: P -> ~R.
+Goal Q \/ E.
+
+(*Write your solution here.*)
+
+Abort.
+End exercise1c.
+
+Section exercise1d.
+Hypothesis pr1: P -> Q.
+Hypothesis pr2: P -> R.
+Hypothesis pr3: Q -> (R -> S).
+Goal P -> S.
+
+(*Write your solution here.*)
+
+Abort.
+End exercise1d.
+
+
+Section exercise1e.
+
+Hypothesis pr1: (P \/ ~ Q) <-> (R /\ S ).
+Hypothesis pr2: E.
+Hypothesis pr3: (~E \/ S).
+Goal R -> Q -> P.
+
+(*Write your solution here.*)
+
+Abort.
+End exercise1e.
 
